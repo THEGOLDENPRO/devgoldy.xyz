4ebd1f40374b6e1f95044203190e506a1ea197fb
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-08-24 00:10:41 +0100

    fixed bug in create aliases script

--- scripts/create_aliases.py
+++ scripts/create_aliases.py
@@ -6,6 +6,7 @@ WARNING: Will only work during building.
 
 import os
 import tomllib
+import pathlib
 
 with open("./config.toml", mode="rb") as file:
     toml = tomllib.load(file)
@@ -13,5 +14,9 @@ with open("./config.toml", mode="rb") as file:
     for page in toml["build"]["aliases"]:
         
         for alias in toml["build"]["aliases"][page]:
-            
+            path = pathlib.Path(f"./build/{alias}/")
+
+            if not path.exists():
+                path.mkdir()
+
             os.symlink(f"./{page}", f"./build/{alias}/")
