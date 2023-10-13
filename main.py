1c6367652cee1be873cc980d4956b932330d3266
Dev Goldy <jassim7256@gmail.com>
2023-10-13 16:15:50 +0100

    v1.1 done

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
 import os
-from devgoldyutils import pprint
 from decouple import config
 from datetime import datetime
 from aiohttp import ClientSession
 from jikan4snek import Jikan4SNEK
 
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 __all__ = ("app",)
 __version__ = "1.1"
@@ -25,6 +24,7 @@ app = FastAPI(
     root_path = ROOT_PATH
 )
 
+RAW_GIT_URL = config("RAW_GIT_URL", "https://raw.githubusercontent.com/THEGOLDENPRO/devgoldy.xyz/main")
 BLOG_API_URL = config("BLOG_API_URL", "https://api.devgoldy.xyz/goldy-exe/v1")
 
 jikan = Jikan4SNEK()
@@ -35,6 +35,7 @@ templates = Jinja2Templates(directory = "./templates")
 async def index(request: Request):
     blog_posts = []
     anime_list = []
+    open_source_projects = []
 
     async with http_client.request("GET", BLOG_API_URL + "/posts") as r:
         if r.ok:
@@ -45,6 +46,10 @@ async def index(request: Request):
                     "date_added": datetime.fromisoformat(post.get("date_added")).strftime("%b %d %Y")
                 } for post in await r.json()
             ]
+
+    async with http_client.request("GET", RAW_GIT_URL + "/os_projects_to_preview.json") as r:
+        if r.ok:
+            open_source_projects = await r.json(content_type = None)
 
     mal_history = await jikan.get("thegoldenpro", "history").users()
     mal_updates = await jikan.get("thegoldenpro", "userupdates").users()
@@ -68,27 +73,23 @@ async def index(request: Request):
             anime_list.append(
                 {
                     "action": action,
-                    "url": anime['entry']['url'],
-                    "title": anime['entry']['title'],
-                    "date": datetime.fromisoformat(anime['date']).strftime('%b %d %Y'),
-                    "date_timestamp": datetime.fromisoformat(anime['date']).timestamp()
+                    "url": anime["entry"]["url"],
+                    "title": anime["entry"]["title"],
+                    "date": datetime.fromisoformat(anime["date"]).strftime("%b %d %Y"),
+                    "date_timestamp": datetime.fromisoformat(anime["date"]).timestamp()
                 }
             )
 
     if mal_history is not None:
-
-        for anime in mal_history["data"]:
-            action = f"Watched episode {anime['increment']} of"
-
-            anime_list.append(
-                {
-                    "action": action,
-                    "url": anime["entry"]["url"],
-                    "title": anime["entry"]["name"],
-                    "date": datetime.fromisoformat(anime["date"]).strftime("%b %d %Y"),
-                    "date_timestamp": datetime.fromisoformat(anime['date']).timestamp()
-                }
-            )
+        anime_list += [
+            {
+                "action": f"Watched episode {anime['increment']} of",
+                "url": anime["entry"]["url"],
+                "title": anime["entry"]["name"],
+                "date": datetime.fromisoformat(anime["date"]).strftime("%b %d %Y"),
+                "date_timestamp": datetime.fromisoformat(anime["date"]).timestamp()
+            } for anime in mal_history["data"]
+        ]
 
     anime_list.sort(key = lambda x: x["date_timestamp"], reverse = True)
 
@@ -96,7 +97,8 @@ async def index(request: Request):
         "home.html", {
             "request": request,
             "blog_posts": blog_posts,
-            "anime_list": anime_list
+            "anime_list": anime_list,
+            "open_source_projects": open_source_projects
         }
     )
 
