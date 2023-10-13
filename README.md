ba2c4efa6670dc5ea0f45c19e2d924def9bd0f58
Dev Goldy <jassim7256@gmail.com>
2023-10-13 02:39:37 +0100

    added local set up instructions and started working on new home menu

--- README.md
+++ README.md
@@ -6,4 +6,40 @@
 
 </div>
 
+## Prerequisites
+- [NodeJS](https://nodejs.org/en)
+- [Python 3.8+](https://www.python.org/) (With **venv** installed)
+- [Make](https://www.gnu.org/software/make/) (Optional, but without it you will need to copy and paste the commands from the Makefile)
+
+## Run Locally
+1. Git clone repo.
+```sh
+git clone https://github.com/THEGOLDENPRO/devgoldy.xyz
+```
+
+2. Create python virtual environment.
+```sh
+cd devgoldy.xyz && python -m venv env
+```
+
+3. Activate that environment.
+```sh
+source env/bin/activate
+```
+Windows:
+```sh
+cd env/Scripts && activate
+```
+
+4. BUILD!!!!
+```sh
+make
+```
+If you don't have GNU make installed, then go into the Makefile and find out how to run those commands yourself. Lmao
+
+5. RUN!!!!
+```sh
+make run
+```
+
 > ### Not much here yet.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}", "./templates/**/*.{html,js}"],
     plugins: [],
     theme: {
         extend: {
             colors: {
-                goldyBluey: "#222930",
+                goldyDarky: {
+                    DEFAULT: "#0e1114",
+                    200: "#0a0b0d",
+                    300: "#0b0d0f",
+                    500: "#0e1114"
+                },
+                goldyGreyy: "#222930",
                 goldyCream: "#fbc689",
                 goldyOrangy: {
                     DEFAULT: "#f5671b",
@@ -15,6 +21,12 @@ module.exports = {
                 },
                 goldyGreen: "#d0f54c"
             }
+        },
+
+        screens: {
+            "mobile": {"max": "430px"},
+            "tablet": {"max": "640px"},
+            "desktop": {"max": "1280px"}
         },
 
         fontFamily: {
