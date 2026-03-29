aa7fbb191db53461e7329fe39a7b206d021d74b7
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2026-03-29 18:56:51 +0100

    refactor: switch to tailwindcss v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,6 @@ module.exports = {
     plugins: [],
     theme: {
         extend: {
-            colors: {
-                exeBlack: "#090b11",
-                exeGray: "#9ca3af",
-                goldyPink: "#fb89ab",
-                goldyDarky: {
-                    DEFAULT: "#0e1114",
-                    200: "#0e1114",
-                    300: "#0b0d0f",
-                    500: "#0a0b0d",
-                },
-                goldyGreyy: "#222930",
-                goldyGreyy: {
-                    DEFAULT: "#222930",
-                    100: "#222930",
-                    300: "#2A2B2C"
-                },
-                goldyCream: {
-                    DEFAULT: "#fbc689",
-                    200: "#fbc689",
-                    800: "theme(colors.orange.50)"
-                },
-                goldyWhite: "#f1f1f1",
-                goldyOrangy: {
-                    DEFAULT: "#f5671b",
-                    100: "#f5671b",
-                    300: "#f57d3d",
-                    800: "#f5be3d"
-                },
-                goldyGreen: "#d0f54c"
-            },
-            dropShadow: {
-                "pinky-drop": "3px 0.5px 1px #fb89ab",
-            },
             animation: {
                 "flicker": "flicker 0.00001s infinite ease-in",
                 "fade-in": "fadeIn ease 5s",
@@ -71,7 +38,7 @@ module.exports = {
             "desktop": {"max": "1280px"}
         },
         fontFamily: {
-            "YanoneKaffeesatz": ["YanoneKaffeesatz"],
+            "yanone-kaffeesatz": ["YanoneKaffeesatz"],
             "hacked": ["Hacked-KerX"],
             "typewriter": ["atwriter"],
             "dosis": ["Dosis"]
